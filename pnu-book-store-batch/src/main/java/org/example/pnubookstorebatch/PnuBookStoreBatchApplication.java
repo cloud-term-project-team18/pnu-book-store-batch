3c55526435ca7bdf851bc81e9fb7b80dd1f1297f
Train0303 <rjsdnxogh55@gmail.com>
2024-05-11 00:01:55 +0900

    Feat: Set Spring Project

---
+++ pnu-book-store-batch/src/main/java/org/example/pnubookstorebatch/PnuBookStoreBatchApplication.java
@@ -0,0 +1,13 @@
+package org.example.pnubookstorebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnuBookStoreBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnuBookStoreBatchApplication.class, args);
+	}
+
+}
